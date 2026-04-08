fbde7339dc0ea44a9f340d29599f803d298e7f67
Ivo Welgraven <ivowelgraven04@gmail.com>
2026-04-08 13:03:32 +0200

    Meta Pixel 1322883923020900 toegevoegd

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,6 +41,28 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <CustomCursor />
         {children}
         <Analytics />
+        {/* Meta Pixel */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1322883923020900');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img height="1" width="1" style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1322883923020900&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
         {/* Google Analytics */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-H3S7TWNT5G"
